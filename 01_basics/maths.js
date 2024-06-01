4e829615437adce3d6f94df5229eef1b50bd8e51
Ayushman Shaw <109733206+ElectronZero@users.noreply.github.com>
2024-06-01 18:08:00 +0000

    About Maths and Numbers

---
+++ 01_basics/maths.js
@@ -0,0 +1,37 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(1));  //upto 1 decimal (100.0)
+
+const otherNumber = 123.8966
+
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString()); //default (us format)
+console.log(hundreds.toLocaleString('en-IN')); //to represent the number in indian format (10,00,000)
+
+// +++++++++++++ Maths +++++++++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+console.log(Math.min(4, 3, 6, 8));
+console.log(Math.max(4, 3, 6, 8));
+
+console.log(Math.random()); //always in the range of 0 to 1
+console.log((Math.random()*10) + 1); //to make it in the range >1 and <10
+console.log(Math.floor(Math.random()*10) + 1); 
+
+const min = 10
+const max = 20
+
+
+//Formula to generate random numbers between min and max
+console.log(Math.floor(Math.random() * (max - min + 1)) + min) 
