71321b4647582d8ef4d4846026c823a2f3050c2a
Ayushman Shaw <109733206+ElectronZero@users.noreply.github.com>
2024-06-01 15:22:56 +0000

    Updating Datatypes

--- 01_basics/datatypes.js
+++ 01_basics/datatypes.js
@@ -11,7 +11,6 @@ console.log("Ayushman")
 
 let name = "Ayushman"
 let age = 18
-let isLoggedIn = false
 let state;
 
 // number => 2 to power 53
@@ -27,3 +26,69 @@ let state;
 
 console.log(typeof undefined); // undefined
 console.log(typeof null); // object
+
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId); //not same
+
+const bigNumber = 3456543576654356754n //way to declare bignit by putting 'n' in last
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "ayushman",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);  //symbol
+console.log(typeof myFunction); //object function
+console.log(typeof myObj); //object
+console.log(typeof id); //symbol
+console.log(typeof heros); //object
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+
+// ------------------------------------------------------------------------
+
+// Stack (Primitive), Heap (Non-Primitive)
+
+let Myname = "Ayushman"
+let anotherName = Myname
+
+anotherName = "ayush"
+
+console.log(Myname); 
+console.log(anotherName);  //stored in Stack (copying not referenced)
+
+let user1 = {
+    email: "user@google.com",
+    Age: 20
+}
+
+let user2 = user1
+
+user2.email = "user@gmail.com"
+
+console.log(user1.email);
+console.log(user2.email);  //stored in Heap (take Reference)
