e64ba55eb447f23ec077edd4f46cd18ef876d4f0
Ayushman Shaw <109733206+ElectronZero@users.noreply.github.com>
2024-06-03 05:34:24 +0000

    IIFE

---
+++ 03_basics/iife.js
@@ -0,0 +1,13 @@
+// Immediately Invoked Function Expressions (IIFE)
+
+//IIFE using function
+(function chai(){
+    // named IIFE
+    console.log(`DB CONNECTED`);
+})(); //-->';' should be put to terminate the IIFE or else it will give ERROR in the next statements
+
+
+//IIFE using arrow function
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+} )('ayushman');
