cc54c7a6d802431cf5d4e7d809e2fe866837bc48
Ayushman Shaw <109733206+ElectronZero@users.noreply.github.com>
2024-06-04 14:50:13 +0000

    for each loops

--- iterations/for-each.js
+++ iterations/for-each.js
@@ -2,17 +2,17 @@ const coding = ["js", "ruby", "java", "python", "cpp"]
 
 coding.forEach( function (val){ //--> call-back function (function without a name),
                                 //any name can be given in the parameters of the call-back function
-    // console.log(val); //prints the array
+    console.log(val); //prints the array
 } )
 
 coding.forEach( (item) => {  //Arrow function can also be used (without a name)
-    // console.log(item);
+    console.log(item);
 } )
 
 function printMe(item){
     console.log(item);
 }
-// coding.forEach(printMe)  //can also work in this way...(only the reference is passed)
+coding.forEach(printMe)  //can also work in this way...(only the reference is passed)
 
 coding.forEach( (item, index, arr)=> { //forEach can have more parameters like these 3
     console.log(item, index, arr);
